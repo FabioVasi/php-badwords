54dbd10d4abbe36304800a00d469cd9b86fc4a8a
FabioVasi <vasifabio96@gmail.com>
2023-10-09 14:58:35 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -25,15 +25,23 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body class="m-2">
     
     <form action="script.php" method="GET">
 
-        <input type="text" name="randomwords" id="randomwords" placeholder="Type here your text...">
+        <div class="form-floating">
+            <textarea class="form-control w-50 mb-3" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea2">Comments</label>
+        </div>
+
+        <input class="form-control w-50 mb-3" type="text" name="randomwords" id="randomwords" placeholder="Type here your text...">
+        
         <button type="submit">Invia</button>
 
     </form>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
