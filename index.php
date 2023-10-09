6f8e11e2c10ed0915e20edae5143d10071bd3811
FabioVasi <vasifabio96@gmail.com>
2023-10-09 14:16:24 +0200

    add ex description

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+
+Gestire il tutto con due file diversi.
+
+- index.php
+- script.php
+
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server tramite il form (occhio a quato detto stamattina sui form e gli attributi necessari).
+
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+*/
+
     var_dump($_GET);
 
 ?>
@@ -14,8 +30,8 @@
     
     <form action="script.php" method="GET">
 
-    <input type="text" name="randomwords" id="randomwords" placeholder="Type here your text...">
-    <button type="submit">Invia</button>
+        <input type="text" name="randomwords" id="randomwords" placeholder="Type here your text...">
+        <button type="submit">Invia</button>
 
     </form>
 
